1d0ee5163696897cfe7fcf928cb4e2ad83c3a2e3
RafaPolit <rafaelpolit@gmail.com>
2016-07-31 17:36:14 -0500

    Added infinite grid to live cells, instead of being bound by borders

--- client/app/scripts/game_of_life/controller.js
+++ client/app/scripts/game_of_life/controller.js
@@ -1,35 +1,74 @@
 'use strict';
 
 angular.module('Game_of_life')
-.controller('game_of_life', function ($scope) {
+.controller('game_of_life', function ($scope, $interval) {
 
   $scope.controller_loaded = 'Game of life loaded!';
 
   $scope.matrix = [];
+  $scope.live_cells = [];
+  $scope.ranges = reset_ranges();
 
   $scope.initialize_matrix = function(options) {
     initialize_empty_matrix(options);
-    initialize_on_cels(options);
+    initialize_live_cels(options);
   };
 
-  $scope.initialize_matrix({
-    rows: 4,
-    cols: 8,
-    on_cells: [ { row: 1, col: 4 }, { row: 2, col: 3 }, { row: 2, col: 4 } ]
-  });
+  // $scope.initialize_matrix({
+  //   rows: 4,
+  //   cols: 8,
+  //   on_cells: [ { row: 1, col: 4 }, { row: 2, col: 3 }, { row: 2, col: 4 } ]
+  // });
+
+  // $scope.initialize_matrix({
+  //   rows: 60,
+  //   cols: 60,
+  //   on_cells: [
+  //     { row: 1, col: 3 },
+  //     { row: 2, col: 1 },
+  //     { row: 2, col: 3 },
+  //     { row: 3, col: 2 },
+  //     { row: 3, col: 3 },
+  //   ]
+  // });
+
+  // $scope.initialize_matrix({ rows: 50, cols: 50, on_cells: 'random' });
 
   $scope.next_generation = function() {
-    $scope.matrix_reference = clone_matrix();
+    $scope.live_cells_reference = [];
+    $scope.ranges = reset_ranges();
 
-    for(var row = 0; row < $scope.matrix.length; row ++) {
-      for(var col=0; col < $scope.matrix[row].length; col ++) {
-        var neighbors_alive = get_neighbors_alive(row, col);
-        determine_cell_next_generation(row, col, neighbors_alive);
+    $scope.live_cells.forEach(function(cell) {
+      $scope.live_cells_reference.push(cell);
+      adjust_ranges(cell);
+    });
+
+    $scope.live_cells = [];
+
+    for(var row = $scope.ranges.min.row; row <= $scope.ranges.max.row; row ++) {
+      for(var col = $scope.ranges.min.col; col <= $scope.ranges.max.col; col ++) {
+        add_live_cells_next_generation(row, col);        
       }
     }
   };
 
+  $scope.live_cell = function(row, col) {
+    return ( $scope.live_cells.indexOf(row + ':' + col) !== -1 );
+  };
+
+  $scope.start_life = function() {
+    $scope.life_event_interval = $interval($scope.next_generation, 600);
+  };
+
+  $scope.stop_life = function() {
+    $interval.cancel($scope.life_event_interval);
+  };
+
   // ---
+
+  function reset_ranges() {
+    return { min: { row: 0, col: 0 }, max: { row: 0, col: 0} };
+  }
 
   function initialize_empty_matrix(options) {
     for(var row = 0; row < options.rows; row ++) {
@@ -40,31 +79,55 @@ angular.module('Game_of_life')
     }
   }
 
-  function initialize_on_cels(options) {
-    options.on_cells.forEach(function(on_cell) {
-      $scope.matrix[on_cell.row][on_cell.col] = 1;
-    });
-  }
-
-  function clone_matrix() {
-    var clone = [];
-    for(var row = 0; row < $scope.matrix.length; row ++) {
-      clone[row] = [];
-      for(var col = 0; col < $scope.matrix[row].length; col ++) {
-        clone[row][col] = $scope.matrix[row][col];
-      }
+  function initialize_live_cels(options) {
+    $scope.live_cells = [];
+    if(options.on_cells !== 'random') {
+      options.on_cells.forEach(function(on_cell) {
+        $scope.live_cells.push(on_cell.row + ':' + on_cell.col);
+      });
     }
 
-    return clone;
+    if(options.on_cells === 'random') {
+      for(var row = 0; row < $scope.matrix.length; row ++) {
+        for(var col = 0; col < $scope.matrix[row].length; col ++) {
+          var on_cell = Math.round(Math.random());
+          if(on_cell) { $scope.live_cells.push(row + ':' + col); }
+        }
+      }
+    }
   }
 
-  function get_neighbors_alive(row, col) {
+  function adjust_ranges(cell) {
+    var cell_coordinates = get_cell_coordinates(cell);
+
+    $scope.ranges.min.row = Math.min(cell_coordinates[0] - 1, $scope.ranges.min.row);
+    $scope.ranges.min.col = Math.min(cell_coordinates[1] - 1, $scope.ranges.min.col);
+    $scope.ranges.max.row = Math.max(cell_coordinates[0] + 1, $scope.ranges.max.row);
+    $scope.ranges.max.col = Math.max(cell_coordinates[1] + 1, $scope.ranges.max.col);
+  }
+
+  function add_live_cells_next_generation(row, col) {
+    var neighbors_alive = determine_neighboards_alive(row, col) ;
+    
+    var cell_alive_previous_generation = ($scope.live_cells_reference.indexOf(row + ':' + col) !== -1);
+    if(neighbors_alive >= 2 && neighbors_alive <= 3 && cell_alive_previous_generation) {
+      $scope.live_cells.push(row + ':' + col);
+    }
+
+    if(neighbors_alive === 3 && !cell_alive_previous_generation) {
+      $scope.live_cells.push(row + ':' + col);
+    }
+  }
+
+  function determine_neighboards_alive(row, col) {
     var neighbors_alive = 0;
 
     for(var row_offset = -1; row_offset < 2; row_offset ++) {
       for(var col_offset = -1; col_offset < 2; col_offset ++) {
         if(row_offset !== 0 || col_offset !== 0) {
-          neighbors_alive = get_offest_neighbor_alive(row, row_offset, col, col_offset, neighbors_alive);
+          if($scope.live_cells_reference.indexOf((row + row_offset) + ':' + (col + col_offset)) !== -1) {
+            neighbors_alive ++;
+          }
         }
       }
     }
@@ -72,21 +135,15 @@ angular.module('Game_of_life')
     return neighbors_alive;
   }
 
-  function get_offest_neighbor_alive(row, row_offset, col, col_offset, neighbors_alive) {
-    try {
-      if($scope.matrix_reference[row+row_offset][col+col_offset]) {
-        neighbors_alive ++;
-      }
-    }
-    catch(error) {}
-    return neighbors_alive;
+  function get_cell_coordinates(cell) {
+    var coordinates = cell.split(':');
+
+    coordinates[0] = Number(coordinates[0]);
+    coordinates[1] = Number(coordinates[1]);
+
+    return coordinates;
   }
 
-  function determine_cell_next_generation(row, col, neighbors_alive) {
-    if(neighbors_alive < 2) { $scope.matrix[row][col] = 0; }
-    if(neighbors_alive > 3) { $scope.matrix[row][col] = 0; }
-    if(neighbors_alive === 3) { $scope.matrix[row][col] = 1; }
-  }
 })
 .config(function ($routeProvider) {
   $routeProvider
--- client/app/scripts/game_of_life/specs/controller_spec.js
+++ client/app/scripts/game_of_life/specs/controller_spec.js
@@ -17,28 +17,38 @@ describe('Controller: game_of_life', function () {
       expect(scope.controller_loaded).toContain('loaded');
     });
 
-    it('should have matrix initialized with default values', function() {
-      expect(scope.matrix).toEqual([
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,0,0,0],
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,0,0,0,0,0],
-      ]);
+    it('should have an empty array of live_cells', function() {
+      expect(scope.live_cells).toEqual([]);
+    });
+
+    it('should have ranges for calculations', function() {
+      expect(scope.ranges.min.row).toBe(0);
+      expect(scope.ranges.min.col).toBe(0);
+      expect(scope.ranges.max.row).toBe(0);
+      expect(scope.ranges.max.col).toBe(0);
     });
   });
 
   describe('initialize_matrix function', function() {
-    it('should initialize the matrix with a predifined state', function() {
+
+    beforeEach(function() {
       scope.initialize_matrix({
         rows: 4,
         cols: 8,
-        on_cells: [ { row: 1, col: 4 }, { row: 2, col: 3 }, { row: 2, col: 4 } ]
+        on_cells: [
+          { row: -3, col: -10 },
+          { row: 1, col: 4 },
+          { row: 2, col: 3 },
+          { row: 2, col: 4 }
+        ]
       });
+    });
 
+    it('should initialize the matrix with a predifined size', function() {
       expect(scope.matrix).toEqual([
         [0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,0,0,0],
-        [0,0,0,1,1,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0],
       ]);
 
@@ -49,23 +59,87 @@ describe('Controller: game_of_life', function () {
       });
 
       expect(scope.matrix).toEqual([
-        [0,1,0,0,0,0],
-        [0,0,1,0,0,0],
-        [0,0,0,1,0,0],
-        [0,0,0,0,1,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
       ]);
+    });
+
+    it('should set the live_cells to the initialized state', function() {
+      expect(scope.live_cells).toContain('-3:-10');
+      expect(scope.live_cells).toContain('1:4');
+      expect(scope.live_cells).toContain('2:3');
+      expect(scope.live_cells).toContain('2:4');
+    });
+
+    describe('Random start', function() {
+
+      beforeEach(function() {
+        scope.initialize_matrix({
+          rows: 4,
+          cols: 6,
+          on_cells: 'random'
+        });
+      });
+
+      it('should start with random live cells', function() {
+        expect(scope.matrix).toEqual([
+          [0,0,0,0,0,0],
+          [0,0,0,0,0,0],
+          [0,0,0,0,0,0],
+          [0,0,0,0,0,0],
+        ]);
+
+        expect(scope.live_cells.length).not.toBe(0);
+      });
+    });
+
+  });
+
+  describe('live_cell Function', function() {
+
+    beforeEach(function() {
+      scope.live_cells = [ '4:3', '-1:10' ];
+    });
+
+    it('should return if coordinates are alive', function() {
+      expect(scope.live_cell(4,3)).toBe(true);
+      expect(scope.live_cell(-1,10)).toBe(true);
+      expect(scope.live_cell(-2,10)).toBe(false);
     });
   });
 
   describe('Compute next generation', function() {
-    it('should modify matrix to show new life', function() {
+
+    beforeEach(function() {
+      scope.initialize_matrix({
+        rows: 4,
+        cols: 8,
+        on_cells: [
+          { row: -3, col: -10 },
+          { row: 1, col: 4 },
+          { row: 2, col: 3 },
+          { row: 2, col: 4 }
+        ]
+      });
+
       scope.next_generation();
-      expect(scope.matrix).toEqual([
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,0,0,0,0,0],
-      ]);
+    });
+
+    it('should set the ranges to one row and col before to min and one row and col above max', function() {
+      expect(scope.ranges.min.row).toBe(-4);
+      expect(scope.ranges.min.col).toBe(-11);
+      expect(scope.ranges.max.row).toBe(3);
+      expect(scope.ranges.max.col).toBe(5);
+    });
+
+    it('should modify live_cells to show new generation', function() {
+      expect(scope.live_cells.length).toBe(4);
+      expect(scope.live_cells).toContain('1:3');
+      expect(scope.live_cells).toContain('1:4');
+      expect(scope.live_cells).toContain('2:3');
+      expect(scope.live_cells).toContain('2:4');
     });
   });
 
@@ -81,14 +155,80 @@ describe('Controller: game_of_life', function () {
 
     it('should modify matrix to show new life', function() {
       scope.next_generation();
-      expect(scope.matrix).toEqual([
-        [0,0,0,0,0],
-        [0,0,1,0,0],
-        [0,0,1,0,0],
-        [0,0,1,0,0],
-        [0,0,0,0,0],
-      ]);
+      expect(scope.live_cells.length).toBe(3);
+      expect(scope.live_cells).toContain('1:2');
+      expect(scope.live_cells).toContain('2:2');
+      expect(scope.live_cells).toContain('3:2');
     });
+  });
+
+  describe('Compute glider', function() {
+
+    beforeEach(function() {
+      scope.initialize_matrix({
+        rows: 4,
+        cols: 5,
+        on_cells: [
+          { row: 1, col: 3 },
+          { row: 2, col: 1 },
+          { row: 2, col: 3 },
+          { row: 3, col: 2 },
+          { row: 3, col: 3 },
+        ]
+      });
+
+      scope.next_generation();
+    });
+
+    it('should modify matrix to show new life', function() {
+
+      expect(scope.live_cells.length).toBe(5);
+      expect(scope.live_cells).toContain('1:2');
+      expect(scope.live_cells).toContain('2:3');
+      expect(scope.live_cells).toContain('2:4');
+      expect(scope.live_cells).toContain('3:2');
+      expect(scope.live_cells).toContain('3:3');
+    });
+
+    it('should respect off-grid life', function() {
+      scope.next_generation();
+      scope.next_generation();
+      scope.next_generation();
+
+      expect(scope.live_cells).toContain('2:4');
+      expect(scope.live_cells).toContain('3:2');
+      expect(scope.live_cells).toContain('3:4');
+      expect(scope.live_cells).toContain('4:3');
+      expect(scope.live_cells).toContain('4:4');
+    });
+  });
+
+  describe('start_life Function', function() {
+
+    beforeEach(function() {
+      spyOn(scope, 'next_generation');
+    });
+
+    it('should start an interval every 0.6 secs', inject(function($interval) {
+      scope.start_life();
+      $interval.flush(6000);
+      expect(scope.next_generation.calls.length).toBe(10);
+    }));
+  });
+
+  describe('stop_life Function', function() {
+
+    beforeEach(function() {
+      spyOn(scope, 'next_generation');
+      scope.start_life();
+    });
+
+    it('should start an interval every 0.6 secs', inject(function($interval) {
+      $interval.flush(3000);
+      scope.stop_life();
+      $interval.flush(3000);
+      expect(scope.next_generation.calls.length).toBe(5);
+    }));
   });
 
   describe('when going to /game_of_life', function() {
